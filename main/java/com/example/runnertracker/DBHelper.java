a225965298aade94a4f910a35a059c9afb3a542c
annakuskova <55314661+annakuskova@users.noreply.github.com>
2022-03-08 14:45:26 +0300

    Add files via upload

---
+++ main/java/com/example/runnertracker/DBHelper.java
@@ -0,0 +1,40 @@
+package com.example.runnertracker;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(Context context) {
+        super(context, "localDB", null, 1);
+    }
+
+    // вызывается, когда вызывается getWritableDatabase, а файл базы данных не существует
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE journey (" +
+                "journeyID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                "duration BIGINT NOT NULL," +
+                "distance REAL NOT NULL," +
+                "date DATETIME NOT NULL," +
+                "name varchar(256) NOT NULL DEFAULT 'Пробежка'," +
+                "rating INTEGER NOT NULL DEFAULT 1," +
+                "comment varchar(256) NOT NULL DEFAULT ''," +
+                "image varchar(256) DEFAULT NULL);");
+
+        db.execSQL("CREATE TABLE location (" +
+                " locationID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                " journeyID INTEGER NOT NULL," +
+                " altitude REAL NOT NULL," +
+                " longitude REAL NOT NULL," +
+                " latitude REAL NOT NULL," +
+                " CONSTRAINT fk1 FOREIGN KEY (journeyID) REFERENCES journey (journeyID) ON DELETE CASCADE);");
+    }
+
+    // вызывается, когда файл базы данных существует, но номер версии, хранящийся в базе данных, ниже, чем переданный в конструкторе
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
